593d86d7fbd63540830b4b1485f8e8bdc09dcd48
Gunnar Beutner <gunnar@beutner.name>
2011-02-20 12:42:43 +0100

    Implemented /sbnc help command.

--- src/plugins/ui.py
+++ src/plugins/ui.py
@@ -18,6 +18,23 @@
 from sbnc.plugin import Plugin, ServiceRegistry
 from sbnc.utils import parse_irc_message
 
+class UIAccessCheck(object):
+    """Helper functions for checking users' access."""
+
+    def anyone(clientobj):
+        """Returns True for any user."""
+
+        return True
+    
+    anyone = staticmethod(anyone)
+    
+    def admin(clientobj):
+        """Returns True for any user who is an admin."""
+ 
+        return ('admin' in clientobj.config and clientobj.config['admin'])
+
+    admin = staticmethod(admin)
+
 class UIPlugin(Plugin):
     """User interface plugin. Provides support for /msg -sBNC <command> and /sbnc <command>"""
 
@@ -30,6 +47,10 @@ class UIPlugin(Plugin):
     def __init__(self, service_registry):
         self._proxy = service_registry.get('info.shroudbnc.services.proxy')
         
+        self.commands = {}
+        self.settings = {}
+        self.usersettings = {}
+        
         # register handlers for existing client connections
         for user in self._proxy.users:
             userobj = self._proxy.users[user]
@@ -39,6 +60,10 @@ class UIPlugin(Plugin):
         
         # make sure new clients also get the event handlers
         self._proxy.client_registration_event.add_handler(self._client_registration_handler)
+        
+        self.register_command('help', self._cmd_help_handler, 'User',
+                              'displays a list of commands or information about individual commands',
+                              'Syntax: help [command]\nDisplays a list of commands or information about individual commands.')
         
     def _client_registration_handler(self, evt, clientobj):
         self._register_handlers(clientobj)
@@ -75,8 +100,7 @@ class UIPlugin(Plugin):
         tokens = parse_irc_message(text, can_have_prefix=False)
                 
         if not self._handle_command(clientobj, tokens[1], tokens[2], False):
-            clientobj.send_message('PRIVMSG', clientobj.me.nick,
-                                   'Unknown command. Try /msg -sBNC help', prefix=UIPlugin._identity)
+            self.send_sbnc_reply(clientobj, 'Unknown command. Try /msg -sBNC help', notice=True)
     
     def _client_sbnc_handler(self, evt, clientobj, nickobj, params):
         """
@@ -91,13 +115,88 @@ class UIPlugin(Plugin):
             return
         
         if not self._handle_command(clientobj, params[0], params[1:], True):
-            clientobj.send_message('NOTICE', clientobj.me.nick, 'Unknown command. Try /sbnc help',
-                                   prefix=UIPlugin._identity)
+            self.send_sbnc_reply(clientobj, 'Unknown command. Try /msg -sBNC help', notice=False)
     
     def _handle_command(self, clientobj, command, params, notice):
         """Handles the command."""
 
+        if command in self.commands:
+            if not self.commands[command]['access_check'](clientobj):
+                return
+
+            self.commands[command]['callback'](clientobj, params, notice)
+            return True
+
+    def register_command(self, name, callback, category, description,
+                         help_text, access_check=UIAccessCheck.anyone):
+        self.commands[name] = {
+            'callback': callback,
+            'category': category,
+            'description': description,
+            'help_text': help_text,
+            'access_check': access_check
+        }
+    
+    def unregister_command(self, name):
+        del self.commands[name]
+    
+    def register_setting(self, name):
         pass
+    
+    def unregister_setting(self, name):
+        pass
+    
+    def register_usersetting(self, name):
+        pass
+    
+    def unregister_usersetting(self, name):
+        pass
+
+    def send_sbnc_reply(self, clientobj, message, notice=False):
+        if notice:
+            type = 'NOTICE'
+        else:
+            type = 'PRIVMSG'
+
+        clientobj.send_message(type, clientobj.me.nick, message, prefix=UIPlugin._identity)
+
+    def _cmd_help_handler(self, clientobj, params, notice):
+        if len(params) > 0:
+            command = params[0]
+            
+            if not command in self.commands or not self.commands[command]['access_check'](clientobj):
+                self.send_sbnc_reply(clientobj, 'There is no such command.', notice)
+                return
+            
+            for line in self.commands[command]['help_text'].split('\n'):
+                self.send_sbnc_reply(clientobj, line, notice)
+        else:
+            self.send_sbnc_reply(clientobj, '--The following commands are available to you--', notice)
+            self.send_sbnc_reply(clientobj, '--Used as \'/sbnc <command>\', or \'/msg -sbnc <command>\'', notice)
+            
+            cmds = {}
+            
+            for command in self.commands:
+                cmdobj = self.commands[command]
+                
+                if not cmdobj['access_check'](clientobj):
+                    continue
+                
+                if not cmdobj['category'] in cmds:
+                    cmds[cmdobj['category']] = {}
+                    
+                cmds[cmdobj['category']][command] = cmdobj
+                
+            for category in cmds:
+                self.send_sbnc_reply(clientobj, '--', notice)
+                self.send_sbnc_reply(clientobj, category + ' commands', notice)
+                
+                for command in cmds[category]:
+                    cmdobj = cmds[category][command]
+                    
+                    self.send_sbnc_reply(clientobj, command +  ' - ' + cmdobj['description'], notice)
+                    
+            self.send_sbnc_reply(clientobj, 'End of HELP.', notice)
 
 sr = ServiceRegistry.get_instance()
 
