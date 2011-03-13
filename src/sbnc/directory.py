c80aa5d19aa3bd96cc0551ef14f7a417941e61ff
Gunnar Beutner <gunnar@beutner.name>
2011-03-13 11:27:34 +0100

    Use simplejson if the json module is not available.

--- src/sbnc/directory.py
+++ src/sbnc/directory.py
@@ -15,7 +15,11 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-import json
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
 from uuid import uuid4
 from sqlalchemy import create_engine, Column, Integer, \
     String, ForeignKey, Index, types, orm
